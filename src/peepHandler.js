6b46fd6a508f335ae1bed764ab57c442de525d2d
NadiaAiraf <aidanfaria@gmail.com>
2018-11-08 13:41:50 +0000

    setting up jest, npm, removing node_modules from git tracking

--- src/peepHandler.js
+++ src/peepHandler.js
@@ -5,8 +5,8 @@ var PeepHandler = function() {
 PeepHandler.prototype.createPeep = function (body, num) {
   newDiv = document.createElement('div')
   newDiv.setAttribute('id','peep' + num)
-  newContent = document.createTextNode(body)
-  newDiv.appendChild(newContent)
+  text = document.createTextNode(body)
+  newDiv.appendChild(text)
   form = document.getElementById('final-div')
   document.body.insertBefore(newDiv, form)
 };
