165ad3e48ef22c533dedebf46bcaac9a6330fb42
NadiaAiraf <aidanfaria@gmail.com>
2018-11-08 15:13:05 +0000

    test that a new peep appears above the old peep

--- spec/peepHandler.test.js
+++ spec/peepHandler.test.js
@@ -6,3 +6,11 @@ test('displays a new test peep', () => {
   pH.createPeep('this is a test', 0)
   expect(document.getElementById('peep0').innerHTML).toEqual('this is a test')
 })
+
+test('the new peep is posted after another div', () => {
+  document.body.innerHTML = "<div id='final-div'></div>"
+  let pH = new peepHandler();
+  pH.createPeep('this is a test', 0)
+  expect(document.getElementsByTagName("div")['0'].innerHTML).toEqual('this is a test')
+  expect(document.getElementsByTagName("div")['1'].innerHTML).toEqual('')
+})
